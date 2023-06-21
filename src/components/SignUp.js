f191246c1d284aebe0486b9b014d7ff692db18e4
jioneee <qoxx3659@gmail.com>
2023-06-21 12:54:53 +0900

    Feat: signUp post 요청

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,62 +1,193 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import axios from 'axios';
 
-import { useState } from 'react'
+import { useState } from 'react';
 
 const SignUpContainer = styled.div`
-padding:20px;
-display: flex;
-flex-direction: column;
-box-sizing: border-box;
-width:200px;
-color:black;
-text-align: center;
-align-items: center;
-
-`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 200px;
+  color: black;
+  text-align: center;
+  align-items: center;
+`;
 
 const SignUpHeader = styled.h1`
-text-align: center;
-`
+  text-align: center;
+`;
 
 const SignUp = () => {
-    const [userEmail, setUserEmail] = useState("");
-    const [userEmailError, setUserEmailError] = useState(false);
-    const [userPassword, setUserPassword] = useState("");
-    const [userPasswordError, setUserPasswordError] = useState(false);
+  const url = 'https://www.pre-onboarding-selection-task.shop'; // Remove trailing slash
+  const [userEmail, setUserEmail] = useState('');
+  const [userEmailError, setUserEmailError] = useState(false);
+  const [userPassword, setUserPassword] = useState('');
+  const [userPasswordError, setUserPasswordError] = useState(false);
 
-    const onChangeUserEmail = (e) => {
-        const userEmailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-        if ((!e.target.value || (userEmailRegex.test(e.target.value)))) setUserEmailError(false);
-        else setUserEmailError(true);
-        setUserEmail(e.target.value);
-    };
+  const onChangeUserEmail = (e) => {
+    const userEmailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (!e.target.value || userEmailRegex.test(e.target.value)) setUserEmailError(false);
+    else setUserEmailError(true);
+    setUserEmail(e.target.value);
+  };
+
+  const onChangeUserPassword = (e) => {
+    const userPasswordRegex = /^(?=.*[0-9]).{8,25}$/;
+    if (!e.target.value || userPasswordRegex.test(e.target.value)) setUserPasswordError(false);
+    else setUserPasswordError(true);
+    setUserPassword(e.target.value);
+  };
+
+  const onClickSubmit = async () => {
+    try {
+      const signUpData = {
+        email: userEmail,
+        password: userPassword,
+      };
+
+      await axios.post(`${url}/auth/signup`, signUpData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      console.log('SignUpData:', signUpData);
+    } catch (error) {
+      console.log('Error:', error.response.data);
+  
+    }
+  };
+
+  return (
+    <SignUpContainer>
+      <SignUpHeader>회원가입</SignUpHeader>
+      이메일
+      <input
+        data-testid="email-input"
+        placeholder="이메일을 입력하세요"
+        type="email"
+        name="userEmail"
+        value={userEmail}
+        onChange={onChangeUserEmail}
+      />
+      {userEmailError && <div className="invalid-input">정확한 이메일을 입력해주세요</div>}
+      비밀번호
+      <input
+        data-testid="password-input"
+        placeholder="비밀번호를 입력하세요"
+        type="password"
+        name="userPassword"
+        value={userPassword}
+        onChange={onChangeUserPassword}
+      />
+      {userPasswordError && <div className="invalid-input">비밀번호는 8자 이상으로 입력해주세요</div>}
+
+      {(userEmailError || userPasswordError) ? (
+        <button data-testid="signup-button" disabled="disabled">
+          회원가입
+        </button>
+      ) : (
+        <button data-testid="signup-button" onClick={onClickSubmit}>
+          회원가입
+        </button>
+      )}
+    </SignUpContainer>
+  );
+};
+
+export default SignUp;
 
 
-    const onChangeUserPassword = (e) => {
-        const userPasswordRegex = /^(?=.*[0-9]).{8,25}$/
-        if ((!e.target.value || (userPasswordRegex.test(e.target.value)))) setUserPasswordError(false);
-        else setUserPasswordError(true);
-        setUserPassword(e.target.value);
-    };
-    return (
+
+// import styled from 'styled-components'
+// import axios from 'axios'
+
+// import { useState, useEffect } from 'react'
+// import { useNavigate } from 'react-router-dom'
+
+
+// const SignUpContainer = styled.div`
+// padding:20px;
+// display: flex;
+// flex-direction: column;
+// box-sizing: border-box;
+// width:200px;
+// color:black;
+// text-align: center;
+// align-items: center;
+
+// `
+
+// const SignUpHeader = styled.h1`
+// text-align: center;
+// `
+
+// const SignUp = () => {
+//   // const navigate = useNavigate();
+//     const url = 'https://www.pre-onboarding-selection-task.shop/'
+//     const [userEmail, setUserEmail] = useState("");
+//     const [userEmailError, setUserEmailError] = useState(false);
+//     const [userPassword, setUserPassword] = useState("");
+//     const [userPasswordError, setUserPasswordError] = useState(false);
+
+
+
+//     const onChangeUserEmail = (e) => {
+//         const userEmailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+//         if ((!e.target.value || (userEmailRegex.test(e.target.value)))) setUserEmailError(false);
+//         else setUserEmailError(true);
+//         setUserEmail(e.target.value);
+//     };
+
+
+//     const onChangeUserPassword = (e) => {
+//         const userPasswordRegex = /^(?=.*[0-9]).{8,25}$/
+//         if ((!e.target.value || (userPasswordRegex.test(e.target.value)))) setUserPasswordError(false);
+//         else setUserPasswordError(true);
+//         setUserPassword(e.target.value);
+//     };
+
+//     const SignUpData = JSON.stringify({
+//       userEmail:userEmail,
+//       userPassword:userPassword,
+//   })
+
+//     const onClickSubmit = async (res) => {
+//         try {
+//           await axios.post(`${url}auth/signup`,SignUpData, { 
+  
+  
+//           })
+//           console.log('userdate', SignUpData)
+//           console.log('res',res)
+//         } catch (e) {
+//             console.log('error', SignUpData)
+//             console.log('res',res)
+//         }
+//       };
+
+
+    
+//     return (
     
 
-            <SignUpContainer>
-               <SignUpHeader>회원가입</SignUpHeader>
-               이메일
-               <input data-testid="email-input" placeholder="이메일을 입력하세요" type="email" value={userEmail} onChange={onChangeUserEmail} />
-               {userEmailError && <div className="invalid-input">정확한 이메일을 입력해주세요</div>}
-               비밀번호
-               <input data-testid="password-input"  placeholder="비밀번호를 입력하세요" type="password" value={userPassword} onChange={onChangeUserPassword} />
-               {userPasswordError && <div className="invalid-input">비밀번호는 8자이상으로 입력해주세요</div>}
+//             <SignUpContainer>
+//                <SignUpHeader>회원가입</SignUpHeader>
+//                이메일
+//                <input data-testid="email-input" placeholder="이메일을 입력하세요" type="email" name='userEmail' value={userEmail} onChange={onChangeUserEmail} />
+//                {userEmailError && <div className="invalid-input">정확한 이메일을 입력해주세요</div>}
+//                비밀번호
+//                <input data-testid="password-input"  placeholder="비밀번호를 입력하세요" type="password" name='userPassword' vlaue={userPassword} onChange={onChangeUserPassword} />
+//                {userPasswordError && <div className="invalid-input">비밀번호는 8자이상으로 입력해주세요</div>}
               
                
-               {(userEmailError || userPasswordError) ? <button data-testid="signup-button" disabled='disabled'>회원가입</button> : <button data-testid="signup-button">회원가입</button>}
-            </SignUpContainer>
+//                {(userEmailError || userPasswordError) ? <button data-testid="signup-button" disabled='disabled'>회원가입</button> : <button data-testid="signup-button" onClick={onClickSubmit}>회원가입</button>}
+//             </SignUpContainer>
 
-    )
+//     )
 
 
-}
+// }
 
-export default SignUp
+// export default SignUp
